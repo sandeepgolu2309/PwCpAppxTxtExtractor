d289ba1bf20062616e8f7a9f7e0c7620c33c1537
sandeepgolu2309 <sandeepsheoran2309@gmail.com>
2025-06-01 19:03:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "16214694"
 api_hash = "7545825d90eb7adae543d59909c73121"
-bot_token = "7571788511:AAFI8lF-jttvUsMzn4ewnGncBz7uu_TgR5M"
+bot_token = "8176359747:AAEtKPdPcoZKQptNewc0W59gR3VgGfyDxp0"
 auth_users = [5543709855]
